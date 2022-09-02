f28051ca8103cc1b1b38677c57708a0e81a60c82
PRAVEEN KUMAR <47114655+PraveenKumarGouri@users.noreply.github.com>
2022-09-02 07:48:12 +0530

    Oops Day1

--- test3.py
+++ test3.py
@@ -1,16 +1,19 @@
-import logging
-logging.basicConfig(filename="test4.log", level=logging.WARNING,format='%(asctime)s %(name)s %(levelname)s %(message)s' )
-def divide(a,b):
-    logging.info("The number entered by under is : %s and %s", a,b)
-    try:
-        logging.info("we are into function")
-        div = a/b
-        logging.info("we have compleated division")
-        logging.info("result of code is %s",div)
-        return div
-    except Exception as e:
-        logging.exception(e)
-        print(e)
+class Person :
+    def __init__(praveen,name,surname,emailID,year_of_birth):
+        praveen.name= name
+        praveen.surname= surname
+        praveen.emailID = emailID
+        praveen.year_of_birth=year_of_birth
 
+    def __init__(praveen,name,surname):
+        praveen.name= name
+        praveen.surname= surname
 
-print(divide(3,0))
+    def age(sudh,current_year):
+        return current_year
+anuj_var = Person("anuj","bandari")
+sudh = Person("sudhanshu ","kumar")
+gargi = Person("gargi","xyz")
+print(anuj_var.age(2022))
+s = "sudh"
+s.upper()
